14ee873b467b511b1ea88f4b329da3a4e14e1b9f
Avazbek Olimov <avazbekolimov722@gmail.com>
2025-06-11 19:57:11 +0300

    added validation for User

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,24 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Entities;
 
 public class User
 {
-    public Guid Guid { get; set; }
+    public required Guid Guid { get; set; }
 
-    public string Login { get; set; } = null!;
+    [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Логин может содержать только латинские буквы и цифры.")]
+    public required string Login { get; set; } = null!;
 
-    public string Password { get; set; } = null!;
+    [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Пароль может содержать только латинские буквы и цифры.")]
+    public required string Password { get; set; } = null!;
 
-    public string Name { get; set; } = null!;
+    [RegularExpression(@"^[a-zA-Zа-яА-ЯёЁ]+$", ErrorMessage = "Имя может содержать только буквы русского/латинского алфавита.")]
+    public required string Name { get; set; } = null!;
 
-    public int Gender { get; set; }
+    [Range(0,2, ErrorMessage = "Пол может иметь только следующие значения: 0 (женщина), 1 (мужчина) или 2 (неизвестно).")]
+    public required int Gender { get; set; }
 
     public DateTime? Birthday { get; set; }
 
     public bool Admin { get; set; }
 
-    public DateTime CreatedOn { get; set; }
+    public required DateTime CreatedOn { get; set; }
 
-    public string CreatedBy { get; set; } = null!;
+    public required string CreatedBy { get; set; } = null!;
 
     public DateTime? ModifiedOn { get; set; }
 
