e780ef0a464fea6f062c2b7ea8a5d467a4b6125a
Avazbek Olimov <avazbekolimov722@gmail.com>
2025-06-11 17:42:27 +0300

    comments removed

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -10,39 +10,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(typeof(UserMappingProfile).Assembly);
 
-// 1. Добавляем DbContext с InMemory-провайдером
 builder.Services.AddDbContext<UserDbContext>(options =>
 {
     options.UseInMemoryDatabase("UserInMemoryDb");
 });
 
-// 2. Регистрируем репозиторий
 builder.Services.AddScoped<IUserRepository, EfUserRepository>();
 
-// 3. Регистрируем MediatR (сканируем сборку Application)
 builder.Services.AddMediatR(Assembly.Load("Application"));
 
-// 4. Добавляем контроллеры
 builder.Services.AddControllers();
 
-// 5. Добавляем Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// ---
-// 6. Сидирование: создадим начального пользователя Admin
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
-    // Если ещё нет пользователя с логином "Admin", добавим его
+    
     if (!db.Users.Any(u => u.Login == "Admin"))
     {
         db.Users.Add(new Domain.Entities.User
         {
             Guid = Guid.NewGuid(),
             Login = "Admin",
-            Password = "Admin123", // пароль простейший
+            Password = "Admin123", 
             Name = "Administrator",
             Gender = 2,
             Birthday = null,
@@ -53,9 +46,7 @@ using (var scope = app.Services.CreateScope())
         db.SaveChanges();
     }
 }
-// ---
 
-// 7. Конфигурируем pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -63,11 +54,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-// Здесь можно добавить middleware, например, логирование, но для простоты не добавляем
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
