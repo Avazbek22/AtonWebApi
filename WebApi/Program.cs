3bc947af684b141ae668c008ef779fd94bb6360c
Avazbek Olimov <avazbekolimov722@gmail.com>
2025-06-12 13:16:31 +0300

    removed top level statements for fixing Integration tests running

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -5,55 +5,69 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using Application.Mapping;
+using WebApi.Swagger;
 
-var builder = WebApplication.CreateBuilder(args);
+// No top level, because integration tests didn't work with top level
 
-builder.Services.AddAutoMapper(typeof(UserMappingProfile).Assembly);
+namespace WebApi;
 
-builder.Services.AddDbContext<UserDbContext>(options =>
+public class Program
 {
-    options.UseInMemoryDatabase("UserInMemoryDb");
-});
-
-builder.Services.AddScoped<IUserRepository, EfUserRepository>();
-
-builder.Services.AddMediatR(Assembly.Load("Application"));
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
-    
-    if (!db.Users.Any(u => u.Login == "Admin"))
+    public static void Main(string[] args)
     {
-        db.Users.Add(new Domain.Entities.User
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAutoMapper(typeof(UserMappingProfile).Assembly);
+
+        builder.Services.AddDbContext<UserDbContext>(options =>
         {
-            Guid = Guid.NewGuid(),
-            Login = "Admin",
-            Password = "Admin123", 
-            Name = "Administrator",
-            Gender = 2,
-            Birthday = null,
-            Admin = true,
-            CreatedOn = DateTime.UtcNow,
-            CreatedBy = "System"
+            options.UseInMemoryDatabase("UserInMemoryDb");
         });
-        db.SaveChanges();
+
+        builder.Services.AddScoped<IUserRepository, EfUserRepository>();
+
+        builder.Services.AddMediatR(Assembly.Load("Application"));
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.OperationFilter<AddCurrentUserHeaderOperationFilter>();
+        });
+
+        var app = builder.Build();
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+    
+            if (!db.Users.Any(u => u.Login == "Admin"))
+            {
+                db.Users.Add(new Domain.Entities.User
+                {
+                    Guid = Guid.NewGuid(),
+                    Login = "Admin",
+                    Password = "Admin123", 
+                    Name = "Administrator",
+                    Gender = 2,
+                    Birthday = null,
+                    Admin = true,
+                    CreatedOn = DateTime.UtcNow,
+                    CreatedBy = "System"
+                });
+                db.SaveChanges();
+            }
+        }
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
     }
 }
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
